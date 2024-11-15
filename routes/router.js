1029a0e977af2e110e7f4066de6007dc5ec4cf60
shahanasherin-p <shahanasherinp062@gmail.com>
2024-11-16 00:27:19 +0530

    v1

---
+++ routes/router.js
@@ -0,0 +1,23 @@
+const express =require('express')
+const jwtMiddleware = require('../middlewares/jwtMiddleware')
+
+
+const userController = require('../controllers/userController')
+const taskController =require('../controllers/taskController')
+const router =new express.Router()
+
+router.post('/register',userController.registerController)
+
+router.post('/login',userController.loginController)
+
+router.post('/add-task',jwtMiddleware,taskController.addTaskController)
+
+router.get('/all-task',jwtMiddleware,taskController.getAllTaskController)
+
+router.put('/tasks/:id/edit-task',jwtMiddleware,taskController.updateTaskController)
+
+router.delete('/tasks/:id/delete-task',jwtMiddleware,taskController.deleteTaskController)
+
+router.get('/tasks/:id', jwtMiddleware, getTaskById);
+
+module.exports = router
